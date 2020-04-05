d003f1bd6de4efecf681cf31bc4b9f8f6704183b
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-04-06 01:06:11 +0900

    make assets folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-const distDir = "./dist/";
+const assets = "./assets/";
 const srcDir = "./src/";
 
 pugSrc = srcDir + '*.php.pug';
@@ -19,13 +19,13 @@ pugToPhp = function () {
         .pipe(rename({
             extname: ''
 		}))
-        .pipe(dest(distDir))
+        .pipe(dest('.'))
 };
 
 exports.pugToPhp = parallel(pugToPhp);
 
 sassSrcDir = srcDir + '/sass/**/*.sass';
-cssDistDir = distDir + 'css/';
+cssDistDir = assets + 'css/';
 
 sassToCss = function () {
 	return src(sassSrcDir)
@@ -41,23 +41,8 @@ sassToCss = function () {
 
 exports.sassToCss = parallel(sassToCss);
 
-
-copyImages = function() {
-	return src([srcDir + 'images/*'])
-		.pipe(dest(distDir + 'images/'));
-};
-exports.copyImages = parallel(copyImages);
-
-copyJs = function() {
-	return src([srcDir + 'js/*'])
-		.pipe(dest(distDir + 'js/'));
-};
-exports.copyJs = parallel(copyJs);
-
 exports.watch = parallel(function() {
 	watch([srcDir], function(cp) {
-		this.copyImages();
-		this.copyJs();
 		this.pugToPhp();
 		this.sassToCss();
 		cp();
---
+++ index.php
@@ -0,0 +1,11 @@
+
+<head>
+  <title>my page</title>
+</head>
+<body>
+  <header> 
+    <h1>test page</h1>
+    <div class="discription">this  is my page</div>
+  </header>
+</body>
+<footer><php echo 'copy rignt Takaaki Iwamoto' ?></footer>
